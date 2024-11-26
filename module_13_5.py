d5f1ce1b478125013ee005390f0997e96fc09016
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-11-27 00:16:46 +0800

    Add files via upload
    2024/01/21 00:00|Домашнее задание по теме "Клавиатура кнопок".

---
+++ module_13_5.py
@@ -0,0 +1,63 @@
+from aiogram import Dispatcher, Bot, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
+
+api = ""
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup()
+
+button_calc = KeyboardButton(text="Рассчитать")
+button_info = KeyboardButton(text="Информация")
+
+kb.add(button_calc)
+kb.add(button_info)
+
+kb.resize_keyboard = True
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text="Рассчитать")
+async def set_age(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5  #Сайт из задания не работал, поэтому искал в инете:  (10 x вес (кг) + 6.25 x рост (см) – 5 x возраст (г) + 5)
+    await message.answer(f'Ваша норма калорий: {calories:.2f} ккал в день.')
+    await state.finish()
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
